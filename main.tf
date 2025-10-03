caf96839be7234e0ac966551661f823957d2f763
Abhishek Gupta <abhishekgupta52811@gmail.com>
2025-10-03 15:07:23 +0530

    Initial commit - cleaned history

---
+++ main.tf
@@ -0,0 +1,59 @@
+provider "azurerm" {
+  features {}
+  subscription_id = var.subscription_id
+  tenant_id       = var.tenant_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+}
+
+resource "azurerm_virtual_network" "Vnet" {
+  name                = var.network_name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  address_space       = var.network_address_space
+}
+
+
+resource "azurerm_subnet" "subnet1" {
+  name                 = var.subnet_name
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = azurerm_virtual_network.Vnet.name
+  address_prefixes     = var.subnet_address_prefix
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = var.nic_name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  ip_configuration {
+    name                          = var.subnet_name
+    subnet_id                     = azurerm_subnet.subnet1.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_windows_virtual_machine" "vm" {
+  name                = var.vm_name
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  size                = var.vm_size
+  admin_username      = var.admin_username
+  admin_password      = var.admin_password
+
+  network_interface_ids = [
+    azurerm_network_interface.nic.id,
+  ]
+
+  os_disk {
+    caching              = var.os_disk_caching
+    storage_account_type = var.os_disk_storage_account_type
+  }
+
+  source_image_reference {
+    publisher = var.source_image_publisher
+    offer     = var.source_image_offer
+    sku       = var.source_image_sku
+    version   = var.source_image_version
+  }
+}
---
+++ provider.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = var.resource_group_name
+    storage_account_name = "forterraform008"
+    container_name       = "fortfstate" 
+    key                  = "terraform.tfstate"
+  }
+}
